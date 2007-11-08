3ece2bdf852174312f1a12e10c4a360bc57b5179
Michael Medin <michael@medin.name>
2007-11-08 20:54:39 +0000

    2007-11-08 MickeM  + Added CheckWMIValue to check result-values from a WMI Query  * Changed so performance values are "collapsed" ie 75362115584 = 70.0G (#89)     If anyone dont want this let me know and I will make an option for it.

--- AutoBuild.h
+++ AutoBuild.h
@@ -2,9 +2,9 @@
 #define AUTOBUILD_H
 // change the FALSE to TRUE for autoincrement of build number
 #define INCREMENT_VERSION TRUE
-#define FILEVER        0,2,7,44
-#define PRODUCTVER     0,2,7,142
-#define STRFILEVER     "0.2.7.44"
-#define STRPRODUCTVER  "0.2.7.142"
-#define STRPRODUCTDATE  "2007-11-06"
+#define FILEVER        0,2,7,51
+#define PRODUCTVER     0,2,7,149
+#define STRFILEVER     "0.2.7.51"
+#define STRPRODUCTVER  "0.2.7.149"
+#define STRPRODUCTDATE  "2007-11-08"
 #endif // AUTOBUILD_H
