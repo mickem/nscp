578241430a35875f39531380f0f7619a30183c97
Michael Medin <michael@medin.name>
2007-11-12 22:19:24 +0000

    finallllly!!!! cross compile for x64 works... hopefully I will get ia64 up tomorrow... :)

--- AutoBuild.h
+++ AutoBuild.h
@@ -2,9 +2,9 @@
 #define AUTOBUILD_H
 // change the FALSE to TRUE for autoincrement of build number
 #define INCREMENT_VERSION TRUE
-#define FILEVER        0,2,7,67
-#define PRODUCTVER     0,2,7,165
-#define STRFILEVER     "0.2.7.67"
-#define STRPRODUCTVER  "0.2.7.165"
-#define STRPRODUCTDATE  "2007-11-10"
+#define FILEVER        0,2,7,72
+#define PRODUCTVER     0,2,7,170
+#define STRFILEVER     "0.2.7.72"
+#define STRPRODUCTVER  "0.2.7.170"
+#define STRPRODUCTDATE  "2007-11-12"
 #endif // AUTOBUILD_H
