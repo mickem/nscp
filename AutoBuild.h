5b9d2479cfe076eba12ff90757c907c6ea50dc4c
Michael Medin <michael@medin.name>
2008-02-20 15:53:23 +0000

    added some missing files...

--- AutoBuild.h
+++ AutoBuild.h
@@ -2,9 +2,9 @@
 #define AUTOBUILD_H
 // change the FALSE to TRUE for autoincrement of build number
 #define INCREMENT_VERSION TRUE
-#define FILEVER        0,3,0,60
-#define PRODUCTVER     0,3,0,60
-#define STRFILEVER     _T("0.3.0.60")
-#define STRPRODUCTVER  _T("0.3.0.60")
-#define STRPRODUCTDATE  _T("2008-02-19")
+#define FILEVER        0,3,0,61
+#define PRODUCTVER     0,3,0,61
+#define STRFILEVER     _T("0.3.0.61")
+#define STRPRODUCTVER  _T("0.3.0.61")
+#define STRPRODUCTDATE  _T("2008-02-20")
 #endif // AUTOBUILD_H
