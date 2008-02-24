953a2afcef7542effc624c7ddeff9c36af1dbf1c
Michael Medin <michael@medin.name>
2008-02-24 09:27:59 +0000

    more project fixes (and 3.1 RC1 builds)

--- trunk/AutoBuild.h
+++ trunk/AutoBuild.h
@@ -2,9 +2,9 @@
 #define AUTOBUILD_H
 // change the FALSE to TRUE for autoincrement of build number
 #define INCREMENT_VERSION TRUE
-#define FILEVER        0,3,0,65
-#define PRODUCTVER     0,3,0,65
-#define STRFILEVER     _T("0.3.0.65")
-#define STRPRODUCTVER  _T("0.3.0.65")
-#define STRPRODUCTDATE  _T("2008-02-23")
+#define FILEVER        0,3,1,2
+#define PRODUCTVER     0,3,1,2
+#define STRFILEVER     _T("0.3.1.2")
+#define STRPRODUCTVER  _T("0.3.1.2")
+#define STRPRODUCTDATE  _T("2008-02-24")
 #endif // AUTOBUILD_H
