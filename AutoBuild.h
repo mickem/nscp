5fda37e91d0a2e2a76b893c73bb27cca61c62cda
Michael Medin <michael@medin.name>
2008-06-24 20:41:09 +0000

    2008-06-24 MickeM  + Added new check (to CheckHelpers): CheckOK: Just return OK (anything passed along will be used as a message).  + Added new check (to CheckHelpers): CheckWARNING: Just return WARN (anything passed along will be used as a message).  + Added new check (to CheckHelpers): CheckCRITICAL: Just return CRIT (anything passed along will be used as a message).  + Added new check (to CheckHelpers): CheckVersion: Just return the nagios version (along with OK status).  * Better error messages in the check service thingy.

--- AutoBuild.h
+++ AutoBuild.h
@@ -2,9 +2,9 @@
 #define AUTOBUILD_H
 // change the FALSE to TRUE for autoincrement of build number
 #define INCREMENT_VERSION TRUE
-#define FILEVER        0,3,3,5
-#define PRODUCTVER     0,3,3,5
-#define STRFILEVER     _T("0.3.3.5")
-#define STRPRODUCTVER  _T("0.3.3.5")
-#define STRPRODUCTDATE  _T("2008-06-14")
+#define FILEVER        0,3,3,11
+#define PRODUCTVER     0,3,3,11
+#define STRFILEVER     _T("0.3.3.11")
+#define STRPRODUCTVER  _T("0.3.3.11")
+#define STRPRODUCTDATE  _T("2008-06-24")
 #endif // AUTOBUILD_H
