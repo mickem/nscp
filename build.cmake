86632db2e184c2c3c77ebf1b6708c5c1e3356300
Michael Medin <michael@medin.name>
2010-06-03 18:43:53 +0000

    Refactored build scripts

--- build.cmake
+++ build.cmake
@@ -4,29 +4,20 @@ SET(Boost_DEBUG 1)
 
 set(Boost_USE_STATIC_LIBS   ON)
 set(BOOST_USE_MULTITHREADED ON)
-
-SET(BOOST_INCLUDEDIR D:/source/include/)
-SET(BOOST_LIBRARYDIR D:/source/lib/x86)
 #SET(BOOST_LIB_SUFFIX vc80-mt)
 #SET(Boost_VERSION 1.40)
 
-
-SET(OPENSSL_INCLUDE_DIR D:/source/include/)
-SET(CMAKE_LIBRARY_PATH D:/source/lib/x86/)
-
-SET(PROTOBUF_INCLUDE_DIR D:/source/protobuf-2.3.0/src)
-SET(PROTOBUF_LIBRARYDIR D:/source/protobuf-2.3.0/vsprojects/Debug)
-SET(PROTOBUF_BINARYDIR D:/source/protobuf-2.3.0/vsprojects/Debug)
-
-#SET(PROTOBUF_LIBRARYDIR C:/src/protobuf-2.3.0/vsprojects/Release)
-#SET(PROTOBUF_BINARYDIR C:/src/protobuf-2.3.0/vsprojects/Release)
+SET(INC_BOOST_INCLUDEDIR C:/source/include)
+SET(INC_BOOST_LIBRARYDIR C:/source/lib/x64)
 
 
-SET(CRYPTOPP_DIR C:/src/lib-src/Crypto++5.6.0)
+SET(INC_OPENSSL_INCLUDEDIR C:/source/include)
+SET(INC_NSCP_LIBRARYDIR C:/source/lib/x64)
 
-SET(CRYPTOPP_SOURCE d:/source/libs-c/crypto++-5.6.0)
+SET(INC_PROTOBUF_DIR "c:/source/libraries/protobuf-2.3.0")
+SET(INC_CRYPTOPP_DIR "c:/source/libraries/cryptopp-5.6.0")
 
 ELSE(WIN32)
 
 ENDIF(WIN32)
-SET(CRYPTOPP_SOURCE d:/source/libs-c/crypto++-5.6.0)
+
