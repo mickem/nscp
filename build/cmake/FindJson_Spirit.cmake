3cb3daf288582cf504f1d633bdc7b767d791eec3
Michael Medin <michael@medin.name>
2013-10-29 23:07:22 +0100

    2013-10-29 MickeM  * Added json_sprit as a submodule to see if this works better.  * Changed so json_sprit now builds using their own cmake file which means it should hopefully be better ready for future versions.

--- build/cmake/FindJson_Spirit.cmake
+++ build/cmake/FindJson_Spirit.cmake
@@ -1,23 +1,34 @@
-# - Find tinyxml2 source/include folder
-# This module finds tinyxml2 if it is installed and determines where 
+# - Find json-spirit source/include folder
+# This module finds json-spirit if it is installed and determines where 
 # the files are. This code sets the following variables:
 #
-#  JSON_SPIRIT_FOUND             - have tinyxml2 been found
-#  JSON_SPIRIT_INCLUDE_DIR       - path to where tinyxml2.h is found
+#  JSON_SPIRIT_FOUND             - have json-spirit been found
+#  JSON_SPIRIT_INCLUDE_DIR       - path to where json-spirit is found
 #
 FIND_PATH(JSON_SPIRIT_INCLUDE_DIR 
 	NAMES json_spirit.h
 	PATHS
-		${JSON_SPRIT_DIR}/json_spirit
-		${JSON_SPRIT_DIR}
+		${CMAKE_SOURCE_DIR}/ext/json-spirit/include/json_spirit
+		${JSON_SPRIT_DIR}/include/json_spirit
+		${NSCP_INCLUDEDIR}
+		)
+FIND_PATH(JSON_SPIRIT_CMAKE 
+	NAMES CMakeLists.txt
+	PATHS
+		${CMAKE_SOURCE_DIR}/ext/json-spirit/build
+		${JSON_SPRIT_DIR}/build
 		${NSCP_INCLUDEDIR}
 		)
 
-IF(JSON_SPIRIT_INCLUDE_DIR)
+IF(JSON_SPIRIT_INCLUDE_DIR AND JSON_SPIRIT_CMAKE)
 	SET(JSON_SPIRIT_FOUND TRUE)
-ELSE(JSON_SPIRIT_INCLUDE_DIR)
+	SET(JSON_SPIRIT_LIBRARY_TYPE "STATIC")
+	SET(JSON_SPIRIT_BUILD_DEMOS FALSE)
+	SET(JSON_SPIRIT_BUILD_TESTS FALSE)
+ELSE(JSON_SPIRIT_INCLUDE_DIR AND JSON_SPIRIT_CMAKE)
 	SET(JSON_SPIRIT_FOUND FALSE)
-ENDIF(JSON_SPIRIT_INCLUDE_DIR)
+ENDIF(JSON_SPIRIT_INCLUDE_DIR AND JSON_SPIRIT_CMAKE)
 MARK_AS_ADVANCED(
   JSON_SPIRIT_INCLUDE_DIR
+  JSON_SPIRIT_CMAKE
 )
