9f6e6aef2b19794b7acf028a6f416d33dd61da43
Michael Medin <michael@medin.name>
2014-06-30 09:11:55 +0200

    More vagrant wrapper script fixes (might actually work now)

--- build/sh/run-vagrant.sh
+++ build/sh/run-vagrant.sh
@@ -1,17 +1,28 @@
 #!/bin/sh
+
+destroy_it ()
+{
+  echo "[::] Destroying virtual machine..."
+  vagrant destroy --force 2>&1 1> /dev/null
+}
+
 cd $1
-echo "Building: $1"
-vagrant destroy --force 2>&1 1> /dev/null
-echo "Old machine destroyed"
+echo "[::] Building: $1"
+destroy_it
+
+echo "[::] Booting new machine"
 vagrant up --no-provision
-echo "Virtual machine booted"
-vagrant provision
-EXIT=$?
-if $EXIT == 0 ; then
-  echo "Build success: $EXIT"
-else
-  echo "Build failed: $EXIT"
+if [ "$?" -ne "0" ] ; then
+  echo "[!!] Create vm failed: $?"
+  exit 1
 fi
-vagrant destroy --force 2>&1 1> /dev/null
-echo "New machine destroyed"
-exit $EXIT
+
+echo "[::] Building NSCP"
+vagrant provision
+if [ "$?" -ne "0" ] ; then
+  echo "[!!] Build failed: $?"
+  destroy_it
+  exit 1
+fi
+destroy_it
+exit 0
