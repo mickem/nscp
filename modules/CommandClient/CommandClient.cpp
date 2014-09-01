c26a9a15186df73939008226ad2e0238c1db1ecb
Michael Medin <michael@medin.name>
2014-09-01 09:15:55 +0200

    Fixed issues for centos/redhat (one unitest still fails but that is not critical)

--- modules/CommandClient/CommandClient.cpp
+++ modules/CommandClient/CommandClient.cpp
@@ -19,6 +19,7 @@
 *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
 ***************************************************************************/
 #include "CommandClient.h"
+#include <iostream>
 
 #include <boost/make_shared.hpp>
 
