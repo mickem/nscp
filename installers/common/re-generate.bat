230ccceeba3842d84d18ecee80c7ba0dc8531be1
Michael Medin <michael@medin.name>
2014-12-01 22:12:21 +0100

    aparently regenerated the wix helper script

--- installers/common/re-generate.bat
+++ installers/common/re-generate.bat
@@ -1,3 +1,3 @@
 "C:/Program Files (x86)/WiX Toolset v3.8\bin\heat.exe" dir ..\..\web -cg WEBResources -gg -out web.wxs
 "C:/Program Files (x86)/WiX Toolset v3.8\bin\heat.exe" dir ..\..\scripts -cg ScriptResources -gg -out scripts.wxs
-"C:/Program Files (x86)/WiX Toolset v3.8\bin\heat.exe" dir "D:\source\build\w32\dist\ext\docs\html" -cg HTMLHelp -gg -out html_help.wxs
+"C:/Program Files (x86)/WiX Toolset v3.8\bin\heat.exe" dir "D:\source\build\x64\dev\ext\docs\html" -cg HTMLHelp -gg -out html_help.wxs
