2444e97444e958b3ee0bc7ea7c65b905801e7aa4
Michael Medin <michael@medin.name>
2014-12-08 22:50:30 +0100

    Changed vagrant build script to copy packages out of VM

--- vagrant/files/build.sh
+++ vagrant/files/build.sh
@@ -1,7 +1,9 @@
-#/bin/sh
+#/bin/bash
 mkdir build
 pushd build
 cmake $* /source/nscp || exit 1
 make package || exit 1
 ctest --output-on-failure || exit 1
+[[ -n $(shopt -s nullglob; echo *.rpm) ]] && cp *.rpm /source/nscp/packages
+[[ -n $(shopt -s nullglob; echo *.deb) ]] && cp *.deb /source/nscp/packages
 #sh copy_package.sh
