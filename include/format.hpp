12deae9cd721f58ed104dbe3bf4a9c27ffe40d09
Michael Medin <michael@medin.name>
2015-01-26 23:45:51 +0100

    Perfdata units (and others) are no longer case sensitive

--- include/format.hpp
+++ include/format.hpp
@@ -330,15 +330,16 @@ namespace format {
 		return ret;
 	}
 	template<class T>
-	inline double convert_to_byte_units(T i, std::string unit) {
+	inline double convert_to_byte_units(T i, const std::string unit) {
+		std::string unit_uc = boost::to_upper_copy(unit);
 		char postfix[] = BKMG_RANGE;
 		int idx = 0;
-		if (unit.size() == 0) {
+		if (unit_uc.size() == 0) {
 			return i;
 		}
 		double cpy = static_cast<double>(i);
 		while (idx<BKMG_SIZE) {
-			if (unit[0] == postfix[idx]) {
+			if (unit_uc[0] == postfix[idx]) {
 				return cpy;
 			}
 			cpy/=1024.0;
