8b33ee52ad0a8efe2eaba8bbed80b5c03ce62025
Michael Medin <michael@medin.name>
2015-04-14 20:44:15 +0200

    Added Checknet module with check_ping command

---
+++ modules/CheckNet/CheckNet.cpp
@@ -0,0 +1,101 @@
+/**************************************************************************
+*   Copyright (C) 2004-2007 by Michael Medin <michael@medin.name>         *
+*                                                                         *
+*   This code is part of NSClient++ - http://trac.nakednuns.org/nscp      *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+***************************************************************************/
+#include "CheckNet.h"
+
+#include <parsers/expression/expression.hpp>
+#include <parsers/filter/modern_filter.hpp>
+#include <parsers/filter/cli_helper.hpp>
+#include <parsers/helpers.hpp>
+
+#include <nscapi/nscapi_program_options.hpp>
+#include <nscapi/nscapi_protobuf_functions.hpp>
+#include <nscapi/nscapi_settings_helper.hpp>
+#include <nscapi/nscapi_helper_singleton.hpp>
+
+#include <net/pinger.hpp>
+
+#include "filter.hpp"
+
+namespace sh = nscapi::settings_helper;
+namespace po = boost::program_options;
+
+
+
+void CheckNet::check_ping(const Plugin::QueryRequestMessage::Request &request, Plugin::QueryResponseMessage::Response *response) {
+	modern_filter::data_container data;
+	modern_filter::cli_helper<ping_filter::filter> filter_helper(request, response, data);
+	std::vector<std::string> hosts;
+	std::string hosts_string;
+	bool total = false;
+	int count = 0;
+	int timeout = 0;
+
+	ping_filter::filter filter;
+	filter_helper.add_options("time > 60 or loss > 5%", "time > 100 or loss > 10%", "", filter.get_filter_syntax(), "unknown");
+	filter_helper.add_syntax("${status}: ${problem_count}/${count} (${problem_list})", filter.get_format_syntax(), "${ip} Packet loss = ${loss}%, RTA = ${time}ms", "${host}", "No hosts found", "%(status): All %(count) hosts are ok");
+	filter_helper.get_desc().add_options()
+		("host", po::value<std::vector<std::string> >(&hosts),	
+		"The host to check (or multiple hosts).")
+		("total", po::bool_switch(&total), "Include the total of all matching hosts")
+		("hosts", po::value<std::string>(&hosts_string),	
+		"The host to check (or multiple hosts).")
+		("count", po::value<int>(&count)->default_value(1),	
+		"Number of packets to send.")
+		("timeout", po::value<int>(&timeout)->default_value(500),	
+		"Timeout in milliseconds.")
+		;
+
+	if (!filter_helper.parse_options())
+		return;
+
+	if (!hosts_string.empty())
+		boost::split(hosts, hosts_string, boost::is_any_of(","));
+
+	if (hosts.empty())
+		return nscapi::protobuf::functions::set_response_bad(*response, "No host specified");
+	if (hosts.size() == 1)
+		filter_helper.show_all = true;
+
+	if (!filter_helper.build_filter(filter))
+		return;
+
+	boost::shared_ptr<ping_filter::filter_obj> total_obj;
+	if (total)
+		total_obj = ping_filter::filter_obj::get_total();
+
+	BOOST_FOREACH(const std::string &host, hosts) {
+		result_container result;
+		for (int i=0;i<count;i++) {
+			boost::asio::io_service io_service;
+			pinger ping(io_service, result, host.c_str(), timeout);
+			ping.ping();
+			io_service.run();
+		}
+		boost::shared_ptr<ping_filter::filter_obj> obj = boost::make_shared<ping_filter::filter_obj>(result);
+		filter.match(obj);
+		if (total_obj)
+			total_obj->add(obj);
+	}
+	if (total_obj)
+		filter.match(total_obj);
+	modern_filter::perf_writer writer(response);
+	filter_helper.post_process(filter, &writer);
+}
---
+++ modules/CheckNet/filter.cpp
@@ -0,0 +1,80 @@
+#include <map>
+#include <list>
+
+#include <boost/bind.hpp>
+#include <boost/assign.hpp>
+#include <boost/make_shared.hpp>
+
+#include <parsers/where.hpp>
+
+#include <simple_timer.hpp>
+#include <strEx.h>
+#include "filter.hpp"
+
+using namespace boost::assign;
+using namespace parsers::where;
+
+
+//////////////////////////////////////////////////////////////////////////
+
+parsers::where::node_type get_percentage(boost::shared_ptr<ping_filter::filter_obj> object, parsers::where::evaluation_context context, parsers::where::node_type subject) {
+	boost::tuple<long long, std::string> value = parsers::where::helpers::read_arguments(context, subject, "%");
+	long long number = value.get<0>();
+	std::string unit = value.get<1>();
+
+	if (unit != "%")
+		context->error("Invalid unit: " + unit);
+	return parsers::where::factory::create_int(number);
+}
+
+
+ping_filter::filter_obj_handler::filter_obj_handler() {
+
+	static const parsers::where::value_type type_custom_pct = parsers::where::type_custom_int_1;
+
+	registry_.add_string()
+		("host", &filter_obj::get_host, "The host name or ip address (as given on command line)")
+		("ip", &filter_obj::get_ip, "The ip address name")
+// 		("version", boost::bind(&filter_obj::get_version, _1), "Windows exe/dll file version")
+// 		("filename", boost::bind(&filter_obj::get_filename, _1), "The name of the file")
+// 		("file", boost::bind(&filter_obj::get_filename, _1), "The name of the file")
+// 		("name", boost::bind(&filter_obj::get_filename, _1), "The name of the file")
+		;
+	registry_.add_int()
+		("loss", type_custom_pct, &filter_obj::get_loss, "Packet loss")
+		("time", type_int, &filter_obj::get_time, "Round trip time in ms")
+		("sent", type_int, &filter_obj::get_sent, "Number of packets sent to the host")
+		("recv", type_int, &filter_obj::get_recv, "Number of packets received from the host")
+		("timeout", type_int, &filter_obj::get_timeout, "Number of packets which timed out from the host")
+		;
+/*
+	registry_.add_human_string()
+		("access", boost::bind(&filter_obj::get_access_s, _1), "")
+		("creation", boost::bind(&filter_obj::get_creation_s, _1), "")
+		("written", boost::bind(&filter_obj::get_written_s, _1), "")
+		;
+		*/
+
+	registry_.add_converter()
+		(type_custom_pct, &get_percentage)
+		;
+
+
+}
+
+void ping_filter::filter_obj::add(boost::shared_ptr<ping_filter::filter_obj> other) {
+	if (!other)
+		return;
+	result.num_send_ += other->result.num_send_;
+	result.num_replies_ += other->result.num_replies_;
+	result.num_timeouts_ += other->result.num_timeouts_;
+	result.time_ += other->result.time_;
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+
+boost::shared_ptr<ping_filter::filter_obj> ping_filter::filter_obj::get_total() {
+	return boost::make_shared<ping_filter::filter_obj>();
+
+}
