aba8eeaad73251ac846fe74184600c42b951d2db
Michael Medin <michael@medin.name>
2015-11-10 21:49:34 +0100

    Fixed so source host names is set in NSCA and Graphite client

--- modules/NSCAClient/NSCAClient.cpp
+++ modules/NSCAClient/NSCAClient.cpp
@@ -131,6 +131,7 @@ bool NSCAClient::loadModuleEx(std::string alias, NSCAPI::moduleLoadMode) {
 			strEx::replace(hostname_, "${host_lc}", dn.first);
 			strEx::replace(hostname_, "${domain_lc}", dn.second);
 		}
+		client_.set_sender(hostname_);
 	} catch (nscapi::nscapi_exception &e) {
 		NSC_LOG_ERROR_EXR("NSClient API exception: ", e);
 		return false;
