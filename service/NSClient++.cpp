148e51cd114086639e733772fa55de465d01b897
Michael Medin <michael@medin.name>
2015-11-18 10:09:11 +0100

    Fixed services showing twice in service list

--- service/NSClient++.cpp
+++ service/NSClient++.cpp
@@ -1259,17 +1259,18 @@ NSClientT::plugin_type NSClientT::find_plugin(const unsigned int plugin_id) {
 	return plugin_type();
 }
 
-NSClientT::plugin_type NSClientT::find_plugin(const std::string key) {
+NSClientT::plugin_type NSClientT::find_plugin(const std::string key_ic) {
+	std::string key = boost::to_lower_copy(key_ic);
 	boost::shared_lock<boost::shared_mutex> readLock(m_mutexRW, boost::get_system_time() + boost::posix_time::milliseconds(5000));
 	if (!readLock.owns_lock()) {
 		LOG_ERROR_CORE("FATAL ERROR: Could not get read-mutex.");
 		return plugin_type();
 	}
 	BOOST_FOREACH(plugin_type plugin, plugins_) {
-		std::string s = plugin->get_alias();
+		std::string s = boost::to_lower_copy(plugin->get_alias());
 		if (s == key)
 			return plugin;
-		s = plugin->getModule();
+		s = boost::to_lower_copy(plugin->getModule());
 		if (s == key)
 			return plugin;
 	}
