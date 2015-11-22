52c7a5687baa281dc12f40dd8168c8c6dc8e4c4c
Michael Medin <michael@medin.name>
2015-11-22 08:00:17 +0100

    Fixed drive letters with single character

--- modules/CheckDisk/check_drive.cpp
+++ modules/CheckDisk/check_drive.cpp
@@ -581,7 +581,10 @@ std::list<drive_container> find_drives(std::vector<std::string> drives) {
 			find_all_volumes(ret, found_drives, helper);
 			find_all_drives(ret, found_drives, helper);
 		} else {
-			ret.push_back(drive_container(d, d, "", true));
+			if (d.length() == 1)
+				ret.push_back(drive_container(d + ":", d , "", true));
+			else
+				ret.push_back(drive_container(d, d, "", true));
 		}
 	}
 	return ret;
