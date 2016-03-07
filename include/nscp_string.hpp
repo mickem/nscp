be9c0a0c28e85a538b220051d2693ec9fb7c8a2d
Michael Medin <michael@medin.name>
2016-03-07 19:24:21 +0100

    Fixed #259 parsing extra space on command line

--- include/nscp_string.hpp
+++ include/nscp_string.hpp
@@ -119,14 +119,18 @@ namespace strEx {
 		template<class T>
 		inline void parse_command(const std::string &cmd_line, T &args) {
 			boost::tokenizer<boost::escaped_list_separator<char>, std::string::const_iterator, std::string > tok(cmd_line, boost::escaped_list_separator<char>('\\', ' ', '\"'));
-			BOOST_FOREACH(std::string s, tok)
-				args.push_back(s);
+			BOOST_FOREACH(std::string s, tok) {
+				if (!s.empty())
+					args.push_back(s);
+			}
 		}
 		inline std::list<std::string> parse_command(const std::string &cmd_line) {
 			std::list<std::string> args;
 			boost::tokenizer<boost::escaped_list_separator<char>, std::string::const_iterator, std::string > tok(cmd_line, boost::escaped_list_separator<char>('\\', ' ', '\"'));
-			BOOST_FOREACH(std::string s, tok)
-				args.push_back(s);
+			BOOST_FOREACH(std::string s, tok) {
+				if (!s.empty())
+					args.push_back(s);
+			}
 			return args;
 		}
 	}
