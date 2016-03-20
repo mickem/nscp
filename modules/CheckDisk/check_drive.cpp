9b0ad2bb51a353d93a57d68e1031430e5c50e388
Michael Medin <michael@medin.name>
2016-03-20 07:19:40 +0100

    to merge with prevous

--- modules/CheckDisk/check_drive.cpp
+++ modules/CheckDisk/check_drive.cpp
@@ -603,7 +603,7 @@ void check_drive::check(const Plugin::QueryRequestMessage::Request &request, Plu
 	double magic;
 
 	filter_type filter;
-	filter_helper.add_options("used > 80%", "used > 90%", "mounted = 1", filter.get_filter_syntax(), "unknown");
+	filter_helper.add_options("used > 80%", "used > 90%", "", filter.get_filter_syntax(), "unknown");
 	filter_helper.add_syntax("${status} ${problem_list}", filter.get_filter_syntax(), "${drive_or_name}: ${used}/${size} used", "${drive_or_id}", "%(status): No drives found", "%(status) All %(count) drive(s) are ok");
 	filter_helper.get_desc().add_options()
 		("drive", po::value<std::vector<std::string>>(&drives),
