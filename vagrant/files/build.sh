f477046023907a96990060cf7f3960102393bd8c
Michael Medin <michael@medin.name>
2016-04-05 06:21:52 +0200

    Updated vagrants so the build

--- vagrant/files/build.sh
+++ vagrant/files/build.sh
@@ -2,8 +2,9 @@
 mkdir build
 pushd build
 cmake $* /source/nscp || exit 1
-make package || exit 1
+make || exit 1
 ctest --output-on-failure || exit 1
+make package || exit 1
 [[ -n $(shopt -s nullglob; echo *.rpm) ]] && cp *.rpm /source/nscp/packages
 [[ -n $(shopt -s nullglob; echo *.deb) ]] && cp *.deb /source/nscp/packages
 #sh copy_package.sh
