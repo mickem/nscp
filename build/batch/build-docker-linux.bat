a0f5a4b9ee6b6d783d1045d08e4ca14e48114299
Michael Medin <michael@medin.name>
2017-08-17 21:30:58 +0200

    Added script to build linux images with docker and fixed centos 7

---
+++ build/batch/build-docker-linux.bat
@@ -0,0 +1,13 @@
+@echo off
+if "%GH_TOKEN%"=="" goto :err
+docker build -f build/docker/centos-6.9/Dockerfile --tag builder .
+docker run --rm -it -e GH_TOKEN=%GH_TOKEN% builder
+docker build -f build/docker/centos-7/Dockerfile --tag builder .
+docker run --rm -it -e GH_TOKEN=%GH_TOKEN% builder
+
+goto :end
+:err
+echo Please specify the github key
+echo set GH_TOKEN=WHATEVER
+
+:end
