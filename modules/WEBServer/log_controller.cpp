43a92422f77f1d14cf677f1167572298daef6d2f
Michael Medin <michael@medin.name>
2017-11-08 22:45:22 +0100

    Updated the log controller a bit

--- modules/WEBServer/log_controller.cpp
+++ modules/WEBServer/log_controller.cpp
@@ -4,7 +4,7 @@
 #include <nscapi/nscapi_protobuf.hpp>
 #include <nscapi/nscapi_protobuf_functions.hpp>
 
-#include <str/xtos.hpp>
+#include <str/utils.hpp>
 
 #include <json_spirit.h>
 
@@ -22,23 +22,31 @@ log_controller::log_controller(boost::shared_ptr<session_manager_interface> sess
 	addRoute("GET", "/?$", this, &log_controller::get_log);
 }
 
+bool is_str_empty(const std::string& m) {
+	return m.empty();
+}
 void log_controller::get_log(Mongoose::Request &request, boost::smatch &what, Mongoose::StreamResponse &response) {
 	if (!session->is_loggedin(request, response))
 		return;
 
-	if (!session->can("logs.get", request, response))
+	if (!session->can("logs.list", request, response))
 		return;
 
 	json_spirit::Object root, log;
 	json_spirit::Array data;
 
+	std::list<std::string> levels;
+	str::utils::split(levels, request.get("level", ""), ",");
 	std::string str_position = request.get("pos", "0");
-	std::size_t pos = str::stox<std::size_t>(str_position);
+	std::size_t pos = str::stox<std::size_t>(str_position, 0);
 	BOOST_FOREACH(const error_handler_interface::log_entry &e, session->get_log_data()->get_errors(pos)) {
+		if (!levels.empty() && std::find(levels.begin(), levels.end(), e.type) == levels.end()) {
+			continue;
+		}
 		json_spirit::Object node;
 		node.insert(json_spirit::Object::value_type("file", e.file));
 		node.insert(json_spirit::Object::value_type("line", e.line));
-		node.insert(json_spirit::Object::value_type("type", e.type));
+		node.insert(json_spirit::Object::value_type("level", e.type));
 		node.insert(json_spirit::Object::value_type("date", e.date));
 		node.insert(json_spirit::Object::value_type("message", e.message));
 		data.push_back(node);
