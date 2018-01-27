890f7ac286ee23ca2db737c1b48ca7326d98f431
Michael Medin <michael@medin.name>
2018-01-27 07:42:54 +0100

    Fixed debian 8 docker build possible fix for #537

---
+++ build/sh/build-debian.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+mkdir /build/src
+mkdir /build/deb
+cd /build/src
+dos2unix /nscp/debian/source/format /nscp/debian/*
+cmake $* /nscp || exit 1
+make package_source || exit 1
+VERSION=`/nscp/build/python/version.py -f /nscp/version.txt -d`
+mv nscp-$VERSION-Source.tar.gz /build/nsclient++_$VERSION.orig.tar.gz
+cd /build/deb
+tar zxvf /build/nsclient++_$VERSION.orig.tar.gz || exit 1
+mv nscp-$VERSION-Source/* .
+rm -rf nscp-$VERSION-Source
+dpkg-buildpackage || exit 1
+mv ../nsclient++*_$VERSION_*.deb /packages
+. /etc/os-release
+ARCH=`uname -m`
+OSVERSION=debian-$VERSION_ID
+VERSION=`/nscp/build/python/version.py -f /nscp/version.txt -d`
+zip -j /packages/NSCP-${VERSION}-${OSVERSION}-${ARCH}.zip /packages/nsclient++*_$VERSION_*.deb
+python /nscp/build/python/upload.py --token ${GH_TOKEN} --release ${VERSION} --file /packages/NSCP-${VERSION}-${OSVERSION}-${ARCH}.zip
