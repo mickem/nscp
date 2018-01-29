e1a3942cf2794b789198af6b191bd89e7aa050ea
Michael Medin <michael@medin.name>
2018-01-29 05:59:21 +0100

    Fixed missing debian dependency and adde ddebian build to script

--- build/batch/build-docker-linux.bat
+++ build/batch/build-docker-linux.bat
@@ -4,6 +4,8 @@ docker build -f build/docker/centos-6.9/Dockerfile --tag builder .
 docker run --rm -it -e GH_TOKEN=%GH_TOKEN% builder
 docker build -f build/docker/centos-7/Dockerfile --tag builder .
 docker run --rm -it -e GH_TOKEN=%GH_TOKEN% builder
+docker build -f build/docker/debian-8/Dockerfile --tag builder .
+docker run --rm -it -e GH_TOKEN=%GH_TOKEN% builder
 
 goto :end
 :err
