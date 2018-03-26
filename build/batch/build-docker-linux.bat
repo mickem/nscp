fb73dcf3ef5abc9f7523f31fbda63107ab69912e
Michael Medin <michael@medin.name>
2018-03-26 22:10:56 +0200

    Added option to build single plattform

--- build/batch/build-docker-linux.bat
+++ build/batch/build-docker-linux.bat
@@ -1,13 +1,20 @@
 @echo off
 if "%GH_TOKEN%"=="" goto :err
+
+if "%1"=="" goto :all
+docker build -f build/docker/%1/Dockerfile --tag builder .
+docker run --rm -it -e GH_TOKEN=%GH_TOKEN% builder
+goto :end
+
+:all
 docker build -f build/docker/centos-6.9/Dockerfile --tag builder .
 docker run --rm -it -e GH_TOKEN=%GH_TOKEN% builder
 docker build -f build/docker/centos-7/Dockerfile --tag builder .
 docker run --rm -it -e GH_TOKEN=%GH_TOKEN% builder
 docker build -f build/docker/debian-8/Dockerfile --tag builder .
 docker run --rm -it -e GH_TOKEN=%GH_TOKEN% builder
-
 goto :end
+
 :err
 echo Please specify the github key
 echo set GH_TOKEN=WHATEVER
