7f7ef064d5b79c2b0bd0e8e21dccab637674e219
Michael Medin <michael@medin.name>
2025-04-13 16:57:26 +0200

    Removed accidental test code

--- service/settings_client.cpp
+++ service/settings_client.cpp
@@ -157,37 +157,7 @@ int nsclient_core::settings_client::show(std::string path, std::string key) {
   }
   return 0;
 }
-#include <wincred.h>
 int nsclient_core::settings_client::list(std::string path) {
-
-  { //--- SAVE
-    const char *password = "brillant";
-    DWORD cbCreds = 1 + strlen(password);
-
-    CREDENTIALW  cred = {0};
-    cred.Type = CRED_TYPE_GENERIC;
-    cred.TargetName = L"FOO/account";
-    cred.CredentialBlobSize = cbCreds;
-    cred.CredentialBlob = (LPBYTE) password;
-    cred.Persist = CRED_PERSIST_LOCAL_MACHINE;
-    cred.UserName = L"paula";
-
-    BOOL ok = ::CredWriteW (&cred, 0);
-    wprintf (L"CredWrite() - errno %d\n", ok ? 0 : ::GetLastError());
-    if (!ok) exit(1);
-  }
-
-  { //--- RETRIEVE
-    PCREDENTIALW pcred;
-    BOOL ok = ::CredReadW (L"NSClient++/account", CRED_TYPE_GENERIC, 0, &pcred);
-    wprintf (L"CredRead() - errno %d\n", ok ? 0 : ::GetLastError());
-    if (!ok) exit(1);
-    wprintf (L"Read username = '%s', password='%S' (%d bytes)\n",
-             pcred->UserName, (char*)pcred->CredentialBlob, pcred->CredentialBlobSize);
-    // Memory allocated by CredRead() must be freed!
-    ::CredFree (pcred);
-  }
-
   try {
     dump_path(path);
   } catch (settings::settings_exception e) {
