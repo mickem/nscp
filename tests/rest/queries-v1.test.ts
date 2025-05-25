47e07d8298d3e39f79c53b99aee04ecac8412a48
Michael Medin <michael@medin.name>
2025-05-25 06:29:29 +0200

    Fixed rest API returning the wrong status code

--- tests/rest/queries-v1.test.ts
+++ tests/rest/queries-v1.test.ts
@@ -122,6 +122,75 @@ describe("queries", () => {
       });
   });
 
+  it("can execute query (json, warning)", async () => {
+    await request(URL)
+      .get(
+        "/api/v1/queries/check_warning/commands/execute?message=this+is+a+message",
+      )
+      .set("Authorization", `Bearer ${key}`)
+      .trustLocalhost(true)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toBeDefined();
+        expect(response.body).toEqual({
+          command: "check_warning",
+          lines: [
+            {
+              message: "this is a message",
+              perf: {},
+            },
+          ],
+          result: 1,
+        });
+      });
+  });
+
+  it("can execute query (json, critical)", async () => {
+    await request(URL)
+      .get(
+        "/api/v1/queries/check_critical/commands/execute?message=this+is+a+message",
+      )
+      .set("Authorization", `Bearer ${key}`)
+      .trustLocalhost(true)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toBeDefined();
+        expect(response.body).toEqual({
+          command: "check_critical",
+          lines: [
+            {
+              message: "this is a message",
+              perf: {},
+            },
+          ],
+          result: 2,
+        });
+      });
+  });
+
+  it("can execute query (json, unknown)", async () => {
+    await request(URL)
+      .get(
+        "/api/v1/queries/check_unknown/commands/execute?message=this+is+a+message",
+      )
+      .set("Authorization", `Bearer ${key}`)
+      .trustLocalhost(true)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toBeDefined();
+        expect(response.body).toEqual({
+          command: "check_unknown",
+          lines: [
+            {
+              message: "Unknown command(s): check_unknown",
+              perf: {},
+            },
+          ],
+          result: 3,
+        });
+      });
+  });
+
   it("can execute query (text)", async () => {
     await request(URL)
       .get("/api/v1/queries/mock_query/commands/execute?a=b&c=d&e=f")
@@ -138,7 +207,7 @@ describe("queries", () => {
       });
   });
 
-  it("can execute query (json)", async () => {
+  it("can execute query (nagios, ok)", async () => {
     await request(URL)
       .get("/api/v1/queries/mock_query/commands/execute_nagios?a=b&c=d&e=f")
       .set("Authorization", `Bearer ${key}`)
@@ -159,7 +228,76 @@ describe("queries", () => {
       });
   });
 
-  it("can execute query (text)", async () => {
+  it("can execute query (nagios, warning)", async () => {
+    await request(URL)
+      .get(
+        "/api/v1/queries/check_warning/commands/execute_nagios?message=this+is+a+message",
+      )
+      .set("Authorization", `Bearer ${key}`)
+      .trustLocalhost(true)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toBeDefined();
+        expect(response.body).toEqual({
+          command: "check_warning",
+          lines: [
+            {
+              message: "this is a message",
+              perf: "",
+            },
+          ],
+          result: "WARNING",
+        });
+      });
+  });
+
+  it("can execute query (nagios, critical)", async () => {
+    await request(URL)
+      .get(
+        "/api/v1/queries/check_critical/commands/execute_nagios?message=this+is+a+message",
+      )
+      .set("Authorization", `Bearer ${key}`)
+      .trustLocalhost(true)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toBeDefined();
+        expect(response.body).toEqual({
+          command: "check_critical",
+          lines: [
+            {
+              message: "this is a message",
+              perf: "",
+            },
+          ],
+          result: "CRITICAL",
+        });
+      });
+  });
+
+  it("can execute query (nagios, unknown)", async () => {
+    await request(URL)
+      .get(
+        "/api/v1/queries/check_unknown/commands/execute_nagios?message=this+is+a+message",
+      )
+      .set("Authorization", `Bearer ${key}`)
+      .trustLocalhost(true)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toBeDefined();
+        expect(response.body).toEqual({
+          command: "check_unknown",
+          lines: [
+            {
+              message: "Unknown command(s): check_unknown",
+              perf: "",
+            },
+          ],
+          result: "UNKNOWN",
+        });
+      });
+  });
+
+  it("can execute query (text, ok)", async () => {
     await request(URL)
       .get("/api/v1/queries/mock_query/commands/execute_nagios?a=b&c=d&e=f")
       .set("Authorization", `Bearer ${key}`)
@@ -172,6 +310,51 @@ describe("queries", () => {
         expect(response.text).toEqual(
           "mock_query::a=b,c=d,e=f|'a label'=30Z;20;30;10;50 'another label'=33Z;20;30\n",
         );
+      });
+  });
+  it("can execute query (text, warning)", async () => {
+    await request(URL)
+      .get(
+        "/api/v1/queries/check_warning/commands/execute_nagios?message=this+is+a+message",
+      )
+      .set("Authorization", `Bearer ${key}`)
+      .set("Accept", "text/plain")
+      .set("Content-Type", "text/plain")
+      .trustLocalhost(true)
+      .expect(202)
+      .expect("Content-Type", "text/plain")
+      .then((response) => {
+        expect(response.text).toEqual("this is a message\n");
+      });
+  });
+  it("can execute query (text, critical)", async () => {
+    await request(URL)
+      .get(
+        "/api/v1/queries/check_critical/commands/execute_nagios?message=this+is+a+message",
+      )
+      .set("Authorization", `Bearer ${key}`)
+      .set("Accept", "text/plain")
+      .set("Content-Type", "text/plain")
+      .trustLocalhost(true)
+      .expect(500)
+      .expect("Content-Type", "text/plain")
+      .then((response) => {
+        expect(response.text).toEqual("this is a message\n");
+      });
+  });
+  it("can execute query (text, unknwon)", async () => {
+    await request(URL)
+      .get(
+        "/api/v1/queries/check_unknown/commands/execute_nagios?message=this+is+a+message",
+      )
+      .set("Authorization", `Bearer ${key}`)
+      .set("Accept", "text/plain")
+      .set("Content-Type", "text/plain")
+      .trustLocalhost(true)
+      .expect(503)
+      .expect("Content-Type", "text/plain")
+      .then((response) => {
+        expect(response.text).toEqual("Unknown command(s): check_unknown\n");
       });
   });
 });
