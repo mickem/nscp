69ef3fb663509b0fddd920f7997dc6db4e3db9b0
Michael Medin <michael@medin.name>
2026-01-30 08:13:09 +0100

    Removed unnecessary file and prefixed libs with nscp to work better on linux.

--- include/parsers/where/dll_defines.hpp
+++ include/parsers/where/dll_defines.hpp
@@ -21,15 +21,15 @@
 
 // We are using the Visual Studio Compiler and building Shared libraries
 #if defined(_WIN32)
-#if defined(where_filter_NOLIB)
+#if defined(nscp_where_filter_NOLIB)
 #define NSCAPI_EXPORT
 #else
-#if defined(where_filter_EXPORTS)
+#if defined(nscp_where_filter_EXPORTS)
 #define NSCAPI_EXPORT __declspec(dllexport)
 #else
 #define NSCAPI_EXPORT __declspec(dllimport)
-#endif /* where_filter_EXPORTS */
-#endif /* where_filter_NOLIB */
+#endif /* nscp_where_filter_EXPORTS */
+#endif /* nscp_where_filter_NOLIB */
 #else  /* defined (_WIN32) */
 #define NSCAPI_EXPORT
 #endif
