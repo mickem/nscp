974a5daf21e58a17a706592b47b8c7e73ffc5fdf
Michael Medin <michael@medin.name>
2026-03-08 14:36:03 +0100

    Reverted nscp_pugin_api name change on windows to prevent installer from breaking

--- include/nscapi/dll_defines.hpp
+++ include/nscapi/dll_defines.hpp
@@ -22,15 +22,15 @@
 // We are using the Visual Studio Compiler and building Shared libraries
 
 #if defined(_WIN32)
-#if defined(nscp_plugin_api_NOLIB)
+#if defined(plugin_api_NOLIB)
 #define NSCAPI_EXPORT
 #else
-#if defined(nscp_plugin_api_EXPORTS)
+#if defined(plugin_api_EXPORTS)
 #define NSCAPI_EXPORT __declspec(dllexport)
 #else
 #define NSCAPI_EXPORT __declspec(dllimport)
-#endif /* nscp_plugin_api_EXPORTS */
-#endif /* nscp_plugin_api_NOLIB */
+#endif /* plugin_api_EXPORTS */
+#endif /* plugin_api_NOLIB */
 #else  /* defined (_WIN32) */
 #if defined(nscp_plugin_api_NOLIB)
 #define NSCAPI_EXPORT
