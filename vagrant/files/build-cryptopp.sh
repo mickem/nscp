916d2de0e4a70918f8d4b0253aac30eca7f07686
Michael Medin <michael@medin.name>
2026-05-15 14:37:44 +0200

    Removed dead files

--- vagrant/files/build-cryptopp.sh
+++
@@ -1,7 +0,0 @@
-mkdir tmp-cryptopp
-pushd tmp-cryptopp/
-wget http://www.cryptopp.com/cryptopp562.zip
-unzip cryptopp562.zip
-make
-sudo make install
-popd
--- vagrant/files/deb-puppet.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-apt-get -y install puppet
