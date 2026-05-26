8ef9b83fbae05e50e1ae0985fdd5c4d087d4a010
Michael Medin <michael@medin.name>
2026-05-26 04:22:27 +0200

    silenced some msbuild warnings

--- libs/mongoose-cpp/mongoose_wrapper.h
+++ libs/mongoose-cpp/mongoose_wrapper.h
@@ -3,6 +3,10 @@
 #ifdef _MSC_VER
 #pragma warning(push)
 #pragma warning(disable : 4201)  // nonstandard extension used: nameless struct/union
+#pragma warning(disable : 4204)  // nonstandard extension used: bit field types other than int
+#pragma warning(disable : 4267)  // conversion from 'size_t' to 'type', possible loss of data
+#pragma warning(disable : 4310)  // cast truncates constant value
+
 #endif
 
 #include <mongoose.h>
